44588d47adecacb2c8f781944a3bb109a905f045
rbestekaya34 <167192001+rbestekaya34@users.noreply.github.com>
2024-05-03 15:03:56 +0200

    Add files via upload

---
+++ CreateScreen.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import BlogPostForm from '../components/BlogPostForm';
+import { useContext } from 'react';
+import { Context } from '../context/BlogContext';
+
+export default function CreateScreen({ navigation }) {
+  const { addBlogPost } = useContext(Context);
+  return (
+    <BlogPostForm
+      isEditable={false}
+      onSubmit={(title, content) => {
+        addBlogPost(title, content, () => navigation.navigate('Index'));
+      }}
+    />
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ jsonServer.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'https://c62a-31-223-64-98.ngrok-free.app/',
+});
